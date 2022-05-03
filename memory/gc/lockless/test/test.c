696c57a3ad0ebfa8d7a74e75d4cb7cb18fb98175
LizBing <f13711735190@163.com>
2022-05-04 00:52:43 +0800

    main

--- memory/gc/lockless/test/test.c
+++ memory/gc/lockless/test/test.c
@@ -1,39 +1,11 @@
 #include "../llgc.h"
 #include "../../../../process/thread.h"
 
+
+
 int main() {
     thrdPoolInit(1);
     llgc_init(2, 1 << 24, malloc(1 << 24));
-
-    atomic_ptrdiff_t ptr = 0;
-    rshdl_t rs = llgc_getRootSet(0);
-    llgc_pushRootSet(rs, &ptr, 1);
-    
-    while(!llgc_timeCount());
-    for(;;) {
-        void* temp = llgc_malloc(128, 0);
-        // if(!temp) return 1;
-        atomic_store(&ptr, temp);
-        printf("%p\n", temp);
-    }
-    
-/*
-    for(int i = 0; i < 1 << 23; i += 168) {
-        atomic_store(&ptr, llgc_malloc(128, 0));
-    }
-*/
-    /*
-    atomic_store(&ptr, llgc_malloc(128, 10));
-    for(int j = 0; j < 100; j++) {
-        printf("round %d:\n", j);
-
-        for(int i = 0; i < 10; i++) {
-            printf("%p\n", llgc_load(&ptr, sizeof(void*) * i));
-            llgc_store(&ptr, sizeof(void*) * i, llgc_malloc(128, 0));
-        }
-    }
-*/
-    
     
     printf("%zu, %zu\n", llgc_allocated(), llgc_collected());
     
